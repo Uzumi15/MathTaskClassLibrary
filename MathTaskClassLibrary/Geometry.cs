3238b2d7a8ddb63f7dae5eb85d67a118651841bc
Student <Student@WIN-30R6SD751UQ>
2026-03-05 10:46:07 +0500

    Добавьте файлы проекта.

---
+++ MathTaskClassLibrary/Geometry.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTaskClassLibrary
+{
+    public class Geometry
+    {
+        public int RectangleArea(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ MathTaskClassLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathTaskClassLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathTaskClassLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("863a3e82-5650-443c-87c1-a5d9455daa26")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
